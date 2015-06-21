46cffd0e952f93d49fa4c9d87052a4aa1f2685f8
NateDrake <john1o1grady@hotmail.com>
2015-06-21 22:50:42 +0100

    Add default model and update psr-4 autoload namespace for models
    Add default model and update psr-4 autoload namespace for models

---
+++ app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class User
+ * @extends \Core\Model
+ * @author John O'Grady
+ * @date 21/06/2015
+ */
+
+namespace Models;
+use \Core\Model as Model;
+class User extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
