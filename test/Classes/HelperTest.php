d559013c8a194afa6b87ff2c533d5a3605496242
NateDrake <john1o1grady@hotmail.com>
2015-06-21 22:17:00 +0100

    Update PHPUnit Testing
    Update HelperTest

--- test/Classes/HelperTest.php
+++ test/Classes/HelperTest.php
@@ -1,15 +1,14 @@
 <?php
 
 /**
- * Created by PhpStorm.
- * User: John
+ * Class HelperTest
+ * @extends PHPUnit_Framework_TestCase
+ * @author John O'Grady
  * Date: 21/06/2015
- * Time: 15:00
  */
 
 class HelperTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testReplaceDirSeparatorReturnsBackSlash()
     {
         $this->assertEquals("\\", \Classes\Helper::replaceDirSeparator('/'));
