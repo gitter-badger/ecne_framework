d104fe24ee3c4035db035a2da6d6fd26e4005242
NateDrake <john1o1grady@hotmail.com>
2015-06-21 22:18:08 +0100

    PHPUnit Test Update
    Update HelperTest

--- test/Classes/HelperTest.php
+++ test/Classes/HelperTest.php
@@ -4,7 +4,7 @@
  * Class HelperTest
  * @extends PHPUnit_Framework_TestCase
  * @author John O'Grady
- * Date: 21/06/2015
+ * @date 21/06/2015
  */
 
 class HelperTest extends \PHPUnit_Framework_TestCase
