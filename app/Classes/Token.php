b1e5e4f57b6eae551a2372b7576d8b71ad973683
NateDrake <john1o1grady@hotmail.com>
2015-06-22 01:47:36 +0100

    Token and Validation
    Token and Validation classes added for handling input from POST and GET
    super GloBals

---
+++ app/Classes/Token.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ *  Class \Classes\Token
+ *  @author John O'Grady
+ *  @date 22/06/2015
+ */
+
+namespace Classes;
+class Token
+{
+    /**
+     * @method generate
+     * @access public
+     * @return string
+     */
+    public static function generate()
+    {
+        return Session::put(Config::get('session/token_name'), md5(uniqid()));
+    }
+
+    /**
+     * @method check
+     * @access public
+     * @param $token
+     * @return bool
+     */
+    public static function check($token)
+    {
+        $tokenName = Config::get('session/token_name');
+        if (Session::exists($tokenName) && $token === Session::get($tokenName)) {
+            Session::delete($tokenName);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}   /** End Class Definition **/
