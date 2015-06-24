b2b67069ecb389b2ea794472e89411c3b3cc99ae
NateDrake <john1o1grady@hotmail.com>
2015-06-24 18:34:56 +0100

    Updating form builder
    Updating core functionality of Form class, changing the way the form is
    build, and how it is rendered

--- app/Classes/Form/Form.php
+++ app/Classes/Form/Form.php
@@ -8,56 +8,147 @@
 
 namespace Classes\Form;
 
+use Classes\Config;
+use \Classes\Token;
+
 class Form
 {
+    /**
+     * @var string
+     */
     private $action;
+    /**
+     * @var string
+     */
     private $method;
-
-    private $formHead;
-    private $formTitle;
-    private $formFoot = '</form>';
-
+    /**
+     * @var string
+     */
+    private $title;
+    /**
+     * @var string
+     */
+    private $banner;
+    /**
+     * @var string
+     */
+    private $token;
+    /**
+     * @var array
+     */
     private $elements = array();
+    /**
+     * @var string
+     */
+    private $output;
 
+    /**
+     * @method construct
+     * @access public
+     * @param $title
+     * @param $action
+     * @param $method
+     */
     public function __construct($title, $action, $method)
     {
-        self::init($title, $action, $method);
+        $this->init($title, $action, $method);
     }
 
+    /**
+     * @method init
+     * @access public
+     * @param $title
+     * @param $action
+     * @param $method
+     */
     public function init($title, $action, $method)
     {
         $this->action = $action;
         $this->method = $method;
-        $this->title = self::parseTitle($title);
-        $this->formHead = '<form action="' . $this->action . '" method="' . $this->method . '">';
-        $this->formHead .= '<h' . $this->title[1] . '>' . $this->title[0] . '</h' . $this->title[1] . '>';
+        $this->setTitle($title);
     }
 
-    public function parseTitle($title)
+    /**
+     * @method setTitle
+     * @access public
+     * @param $title
+     */
+    public function setTitle($title)
     {
-        if (preg_match('/|/', $title)) {
-            return (explode("|", $title));
+        if (preg_match('/[a-zA-Z0-9]{1,}[|]{1}[1-5]{1}/', $title, $matches)) {
+            $this->title = (explode('|', $matches)[1]);
+            $this->banner = (explode('|', $matches)[0]);
+        } else {
+            $this->title = '2';
+            $this->banner = $title;
         }
     }
 
+    /**
+     * @method addElement
+     * @access public
+     * @param $element
+     */
     public function addElement($element)
     {
         array_push($this->elements, $element);
     }
 
+    /**
+     * @method getElements
+     * @access public
+     * @return array
+     */
     public function getElements()
     {
         return $this->elements;
     }
 
-    public function build()
+    /**
+     * @method buildFormHeader
+     * @access public
+     * @return string
+     */
+    public function buildFormHeader()
     {
-        $this->formHead .= '<input type="hidden" name="token" value="' . \Classes\Token::generate() . '" />';
-        $form = $this->formHead;
-        foreach ($this->elements as $element) {
-            $form .= $element->render();
-        }
-        $form .= $this->formFoot;
-        echo $form;
+        return '<form action="' . $this->action . '"><h' . $this->title . '>' . $this->banner . '</h' . $this->title .'>';
+    }
+
+    /**
+     * @method buildToken
+     * @access public
+     * @return string
+     */
+    public function buildToken()
+    {
+        $this->token = Token::generate();
+        return '<input type="hidden" name="' . Config::get('token/name') . '" value="' . $this->token . '" />';
+    }
+
+    public function buildFormFooter()
+    {
+        return '</form>';
+    }
+
+    public function splash($output)
+    {
+        ob_start();
+        echo $output;
+        $this->output = ob_get_contents();
+        ob_end_clean();
+        echo $this->output;
+    }
+
+    /**
+     * @method render
+     * @access public
+     */
+    public function render()
+    {
+        $this->output = join(" ", array(
+            $this->buildFormHeader(),
+            $this->buildToken(),
+            $this->buildFormFooter()
+        ));
     }
 }
